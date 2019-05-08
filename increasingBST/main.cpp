96fdaf2bf2ea68033c681a097720c9fc77762610
lee <690925801@qq.com>
2019-05-09 00:28:15 +0800

    fix wrong with build tree

--- increasingBST/main.cpp
+++ increasingBST/main.cpp
@@ -14,8 +14,21 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode* increasingBST(TreeNode* root) {
+    if(root == NULL)
+        return NULL;
+//    if(root->left != NULL)
+
+//    if(root->left != NULL)
+        increasingBST(root->left);      //keep run to lefest the output the root
+
+     cout<<"  "<<root->val<<endl;
+
+//    if(root->right != NULL)
+     increasingBST(root->right);
 
     }
+
+
 };
 
 
@@ -31,25 +44,42 @@ int main()
     TreeNode* root = new TreeNode(5);
     res.push_back(root);
     total_res.push_back(res);
-    while(counter <= sizeof(num)/sizeof(int))
+    while(counter < sizeof(num)/sizeof(int))
     {
         vector<TreeNode*> tmp;
-        for(int i=0;i<pow(2,total_res.size());i++)       //before have build n floor,next to build n+1 floor
+        for(int i=0;i<2*total_res.size();i++)       //before have build n floor,next to build n+1 floor
         {
-            TreeNode* tmp_root = new TreeNode(num[counter]);
-            counter++;
-            tmp.push_back(tmp_root);
+            if(num[counter] != NULL)
+            {
+                TreeNode* tmp_root = new TreeNode(num[counter]);
+                counter++;
+                tmp.push_back(tmp_root);
+            }
+            else {
+                TreeNode* tmp_root = NULL;
+                counter++;
+                tmp.push_back(tmp_root);
+            }
         }
         total_res.push_back(tmp);
     }
 
+    cout<<"here"<<endl;
+
     for(int i = 0;i< total_res.size()-1;i++)
     {
-        for(int j = 0;j<total_res[i].size();j++)
+         for(int j = 0;j<total_res[i].size();j++)
         {
-            total_res[i][j]->left = total_res[i+1][j*2];
-            total_res[i][j]->right = total_res[i+1][j*2+1];
+            if(total_res[i][j] != NULL)
+            {
+                total_res[i][j]->left = total_res[i+1][j*2];
+                total_res[i][j]->right = total_res[i+1][j*2+1];
+            }
         }
     }
+
+    Solution test;
+    test.increasingBST(root);
+
     return 0;
 }
